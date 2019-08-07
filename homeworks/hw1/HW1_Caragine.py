96c066df594a3203bdc5a72e75af1bd76638b4b0
ccaragine <ccaragine@wustl.edu>
2019-08-07 16:02:14 -0500

    HW1
    Created Homework 1

---
+++ homeworks/hw1/HW1_Caragine.py
@@ -0,0 +1,16 @@
+############ HOMEWORK 1 ####################
+## Crystal Caragine
+## 8/7/2019
+## HW1_Caragine
+
+class Portfolio:
+    def __init__(self, cash=0, mf=0, s=0):
+        self.cash = cash
+        self.mf = mf
+        self.s = s
+    
+    def show(self) :
+        return 
+        
+port = Portfolio() 
+
