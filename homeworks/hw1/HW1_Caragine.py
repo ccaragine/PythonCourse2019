8c4618f0590c04212e6f38c1577ff662d81fa0d8
ccaragine <ccaragine@wustl.edu>
2019-08-07 21:31:45 -0500

    Update HW1
    sleved problem 1 through 3, 4 to 6 not working

--- homeworks/hw1/HW1_Caragine.py
+++ homeworks/hw1/HW1_Caragine.py
@@ -9,8 +9,44 @@ class Portfolio:
         self.mf = mf
         self.s = s
     
-    def show(self) :
-        return 
+    def show(self, cash, mf, s) :
+        print(mf)
+        print(s)
+        return port.cash
+
+    def stock(self, symbol, amount):
+        self.symbol = symbol 
+        self.amount = amount
         
-port = Portfolio() 
+    def mutualfund(self, symbol, amount):
+        self.symbol = symbol
+        self.amount = amount
+    
+    def __add__(self,cash):
+        self.cash + cash
+        
+    def __sub__(self, cash):
+        self.cash - cash
+        
+    def buystock(self, stock):    
+        self.stock + stock 
+        self.cash - stock 
+        
+    
+       
+        
+port = Portfolio()     # creating portfolio
+
+port = Portfolio(cash=15, mf=20.0, s=4)  # Setting values for port
+port.show(15, 20.0, 4)
+
+port.cash + 300.5         # adding cash 
+
+S = port.stock("HFH", 20)
+
+port.buystock(5)    ##### Not working trying to buy stock
+
+MF1 = port.mutualfund("BRT", 0)     ##### Not working 
+MF2 = port.mutualfund("GHT", 0)
+
 
