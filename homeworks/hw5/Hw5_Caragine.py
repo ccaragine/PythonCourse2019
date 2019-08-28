379f5f2abcfe7b919557b027bd1e897d2c3b2c10
ccaragine <ccaragine@wustl.edu>
2019-08-28 08:57:16 -0500

    Create HW 5

---
+++ homeworks/hw5/Hw5_Caragine.py
@@ -0,0 +1,34 @@
+############## Homework 5 ##############
+## Crystal Caragine
+## 8/25/2019
+## Hw5_Caragine
+
+Class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value 
+        self.next = none
+    
+    def __str__(self):
+        return str(self.value)
+
+Class newlist:    
+
+    def addNode(self, newvalue):
+        new_node = Node(new_value, self.root)
+        self.root = new_node 
+        self.size = 1 
+
+    def  addNodeAfter(self, newvalue, afternode):
+        self.newvalue = newvalue 
+        self.afternode = addNode += afternode 
+        
+     def addNodebefore(self, new_value, before_node): 
+         self.new_value = new_value
+         self.before_node = addNode += before_node 
+         
+      def  removeNode(self, node_to_remove): 
+          self.node_to_remove = 
+          
+      def  removeNodesByValue(self, value): 
+          self.value = Node -= value 
+        
